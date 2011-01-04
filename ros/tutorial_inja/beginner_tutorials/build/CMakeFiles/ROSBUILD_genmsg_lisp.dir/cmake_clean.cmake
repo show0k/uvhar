60104e404091a2d3540a7d0231418079f5524c9c
Inja <Inja@localhost>
2011-01-04 15:28:09 +0100

    inja's ros tutorial finished

---
+++ ros/tutorial_inja/beginner_tutorials/build/CMakeFiles/ROSBUILD_genmsg_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ROSBUILD_genmsg_lisp"
+  "../msg/lisp/beginner_tutorials/Num.lisp"
+  "../msg/lisp/beginner_tutorials/_package.lisp"
+  "../msg/lisp/beginner_tutorials/_package_Num.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ROSBUILD_genmsg_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
