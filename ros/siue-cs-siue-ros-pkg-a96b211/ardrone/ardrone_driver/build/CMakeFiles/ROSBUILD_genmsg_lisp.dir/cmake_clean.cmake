095369a1e8836ab57119a114910409cac2cb5739
Inja <Inja@localhost>
2011-01-04 17:27:20 +0100

    first attempts take off

---
+++ ros/siue-cs-siue-ros-pkg-a96b211/ardrone/ardrone_driver/build/CMakeFiles/ROSBUILD_genmsg_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ROSBUILD_genmsg_lisp"
+  "../msg/lisp/ardrone_driver/Velocity.lisp"
+  "../msg/lisp/ardrone_driver/_package.lisp"
+  "../msg/lisp/ardrone_driver/_package_Velocity.lisp"
+  "../msg/lisp/ardrone_driver/ARDroneState.lisp"
+  "../msg/lisp/ardrone_driver/_package.lisp"
+  "../msg/lisp/ardrone_driver/_package_ARDroneState.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ROSBUILD_genmsg_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ ros/siue-cs-siue-ros-pkg-a96b211/ardrone/ardrone_driver/build/CMakeFiles/ROSBUILD_gensrv_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ROSBUILD_gensrv_lisp"
+  "../srv/lisp/ardrone_driver/Takeoff.lisp"
+  "../srv/lisp/ardrone_driver/_package.lisp"
+  "../srv/lisp/ardrone_driver/_package_Takeoff.lisp"
+  "../srv/lisp/ardrone_driver/Reset.lisp"
+  "../srv/lisp/ardrone_driver/_package.lisp"
+  "../srv/lisp/ardrone_driver/_package_Reset.lisp"
+  "../srv/lisp/ardrone_driver/Land.lisp"
+  "../srv/lisp/ardrone_driver/_package.lisp"
+  "../srv/lisp/ardrone_driver/_package_Land.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ROSBUILD_gensrv_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
