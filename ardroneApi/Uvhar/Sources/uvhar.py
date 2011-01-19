b4c332f249c5ea0c39ee76645f63f64b923c1598
Inja <Inja@localhost>
2011-01-19 13:40:44 +0100

    histograms ><

--- ardroneApi/Uvhar/Sources/uvhar.py
+++ ardroneApi/Uvhar/Sources/uvhar.py
@@ -1,4 +1,5 @@
 import Image, ImageDraw, time
+import sys
 from opencv.cv import *
 from opencv.highgui import *
 
@@ -13,12 +14,23 @@ class Uvhar:
      counter = -1 
 
      # Images 
-     image = None
-     imageH = None
-     imageS = None
-     imageV = None
+     #image = None
+     #imageH = None
+     #imageS = None
+     #imageV = None
      tempResultImage = None
      resultImage = None
+
+     # histogram of what we are looking for
+     sourceHistogram = None
+     hBins = 30; # python api example
+     sBins = 32; # python api example
+     vBins = 32; # 32 because.. why not?
+     # hue varies from 0 (~0 deg red) to 180 (~360 deg red again), python api example 
+     hRange = [0, 180]
+     # saturation varies from 0 (black-gray-white) to 255 (pure spectrum color), python api example
+     sRange = [0, 255]
+     vRange = [0, 255] # I think this applies for value as well
 
      # width and height of the image, we might need to change this
      # when we switch to the bottom camera (or we can detect it, somehow)
@@ -40,11 +52,26 @@ class Uvhar:
 
      def __init__(self):
          print "Uvhar class contructor called.\n"
-         self.imageH = cvCreateImage(cvSize(self.imageWidth, self.imageHeight), 8, 1)
-         self.imageS = cvCreateImage(cvSize(self.imageWidth, self.imageHeight), 8, 1)
-         self.imageV = cvCreateImage(cvSize(self.imageWidth, self.imageHeight), 8, 1)
+         #self.imageH = cvCreateImage(cvSize(self.imageWidth, self.imageHeight), 8, 1)
+         #self.imageS = cvCreateImage(cvSize(self.imageWidth, self.imageHeight), 8, 1)
+         #self.imageV = cvCreateImage(cvSize(self.imageWidth, self.imageHeight), 8, 1)
          self.tempResultImage = cvCreateImage(cvSize(self.imageWidth, self.imageHeight), 8, 3)
          self.resultImage = cvCreateImage(cvSize(self.imageWidth, self.imageHeight), 8, 1)
+
+         # create histogram
+         self.sourceHistogram = cvCreateHist([self.hBins, self.sBins], CV_HIST_ARRAY, [self.hRange, self.sRange])
+         # load image and convert to hsv
+         sourceImage = cvLoadImage("pinkObject.jpg", CV_LOAD_IMAGE_COLOR)
+         Ihsv = cvCreateMat(sourceImage.rows, sourceImage.cols, CV_8UC3)
+         cvCvtColor(sourceImage, Ihsv, CV_BGR2HSV)
+         # extract h, s and v planes
+         hPlane = cvCreateMat(sourceImage.rows, sourceImage.cols, CV_8UC1)
+         sPlane = cvCreateMat(sourceImage.rows, sourceImage.cols, CV_8UC1)
+         vPlane = cvCreateMat(sourceImage.rows, sourceImage.cols, CV_8UC1)
+         cvSplit(Ihsv, hPlane, sPlane, None, None)
+         planes = [hPlane, sPlane]
+         # calculate histogram
+         cvCalcHist(planes, self.sourceHistogram)
 
          # already open the windows :O
          cvNamedWindow(self.mainWindowName) # if window does not resize automagically add CV_WINDOW_AUTOSIZE
@@ -90,6 +117,9 @@ class Uvhar:
              print "result image is null\n"
              return None
 
+              
+
+         """
          # convert to hsv
          cvCvtColor(self.image, self.tempResultImage, CV_BGR2HSV) 
 
@@ -105,6 +135,8 @@ class Uvhar:
          # are range on every channel 
          cvAnd(self.imageH, self.imageS, self.resultImage)
          cvAnd(self.resultImage, self.imageV, self.resultImage)
+         """
+         
 
      def setExitOnNextUpdate(self, event, x, y, flags, param):
          if (event == CV_EVENT_RBUTTONDOWN):
